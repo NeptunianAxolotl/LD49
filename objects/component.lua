3ad38d5b6941d614a1ccc87ea4f5be8e1d305a75
keven ren <ren.keven@gmail.com>
2021-10-02 20:58:45 +1000

    TemporaryFix

--- objects/component.lua
+++ objects/component.lua
@@ -7,22 +7,27 @@ local DEF = {
 	density = 1,
 }
 
-local function RandomCut(sections, mag)
-	local cuts = {}
-	for i = 1, sections do
-		cuts[i] = love.math.random() * mag
+local function RandomCut(sections)
+	local cuts = {0}
+	for i = 1, sections-1 do
+		cuts[i + 1] = love.math.random() * 2*math.pi
 	end
 	table.sort(cuts)
+
+	if cuts[#cuts] < 6*math.pi/5 then
+		print(cuts[#cuts])
+		cuts = util.ScaleArray(cuts, math.random()/2 + 7/6)
+	end
 	return cuts
 end
 
 local function ArbitraryBlock(sides)
 	local block = {}
-	local angles = RandomCut(sides, 2*math.pi)
+	local angles = RandomCut(sides)
 	local magnitudes = {}
 
 	for i = 1, #angles do
-		magnitudes[i] = love.math.random()
+		magnitudes[i] = math.min(love.math.random(), 0.5)
 	end
 
 	local invMaxMag = 1/math.max(unpack(magnitudes))
@@ -42,11 +47,9 @@ local function SetupPhysicsBody(self, physicsWorld)
 	self.shapes = {}
 	self.fixtures = {}
 
-	local angle = util.GetRandomAngle()
 	local modCoords = {}
 	for i = 1, #self.coords do
 		local pos = util.Mult(self.def.minSize + math.random()*(self.def.maxSize - self.def.minSize), self.coords[i])
-		pos = util.RotateVector(pos, angle)
 		modCoords[#modCoords + 1] = pos[1]
 		modCoords[#modCoords + 1] = pos[2]
 		self.coords[i] = pos
