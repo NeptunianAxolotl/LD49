4f5d6a8cd5f4f93b62fc2004726b53898a93b449
GoogleFrog <googl3frog@gmail.com>
2021-10-02 11:48:49 +1000

    Skeleton gets dollar.

--- moduleTest.lua
+++ moduleTest.lua
@@ -2,6 +2,7 @@
 local Resources = require("resourceHandler")
 local EffectsHandler = require("effectsHandler")
 local SoundHandler = require("soundHandler")
+local util = require("include/util")
 
 local self = {}
 local animDt = 0
@@ -12,6 +13,13 @@ function self.Update(dt)
 		SoundHandler.PlaySound("health_down")
 		EffectsHandler.SpawnEffect("health_down", {0, 0})
 		EffectsHandler.SpawnEffect("fireball_explode", {math.random()*500, math.random()*500})
+	end
+	
+	if math.random() < 0.02 then
+		local point = util.RandomPointInEllipse(400, 400)
+		local moneyValue = math.random()*150 + 50
+		EffectsHandler.SpawnEffect("mult_popup", point, {velocity = {0, (-0.55 - math.random()*0.2) * (0.4 + 0.6*(50 / math.max(50, moneyValue)))}, text = "$" .. moneyValue})
+		SoundHandler.PlaySound("coin_collect_2")
 	end
 end
 
