1454064fbb5483f7d4b978d449603a208b238af0
GoogleFrog <googl3frog@gmail.com>
2021-10-04 01:55:58 +1100

    Tweak island and waves.

--- resources/defs/waves.lua
+++ resources/defs/waves.lua
@@ -2,9 +2,9 @@ return {
 	name = "waves",
 	file = "resources/images/wave_1.png",
 	form = "image",
-	xScale = 0.62,
-	yScale = 0.95,
+	xScale = 0.5,
+	yScale = 0.8,
 	width = 400,
 	xOffset = 0,
-	yOffset = 0.55,
+	yOffset = 0.35,
 }
