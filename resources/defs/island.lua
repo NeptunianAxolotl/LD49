bc2c1558fb52bed17d0fe597989c955fc4589b51
GoogleFrog <googl3frog@gmail.com>
2021-10-03 19:17:16 +1100

    Somewhat recentre island.

--- resources/defs/island.lua
+++ resources/defs/island.lua
@@ -2,10 +2,10 @@ return {
 	name = "island",
 	file = "resources/images/island.png",
 	form = "image",
-	xScale = 0.52,
-	yScale = 0.5,
+	xScale = 0.62,
+	yScale = 0.95,
 	width = 400,
-	xOffset = 0.096,
+	xOffset = 0,
 	yOffset = 0.55,
 	duration = 0.4,
 }
