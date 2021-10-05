7ca3be664a517bd4c1fcd9da45bdcd4f8534d032
GoogleFrog <googl3frog@gmail.com>
2021-10-05 18:20:43 +1100

    Keep pink loss text.

--- defs/chatProgression.lua
+++ defs/chatProgression.lua
@@ -70,6 +70,7 @@ local chatProgression = {
         [100] = {
             text = {"Small boats crewed by people", "holding golf clubs have been", "sighted in the distance.",  "Your accountants have taken the", "liberty of calling the company", "lawyers in your stead. You have", "been 'strongly encouraged' to", "take the next available flight", "off the island, in case things", "escalate.", "", "You have lost the game.", "Press Ctrl+R to restart.", ""},
             sound = "chat_very_bad",
+            color = {255/255, 220/255, 220/255},
 			last = true,
         },
 	},
@@ -109,6 +110,7 @@ local chatProgression = {
 		[100] = {
             text = {"Your performance review has concluded,", "and we have decided to find a new", "candidate for your position. The", "budget will certainly not accommodate", "a helicopter following your poor performance.",  "I am afraid you will have to swim", "home.", "", "If your replacement finds you still", "present on the island, they have", "been authorized to evict you with","extreme prejudice.", "", "You have lost the game.", "Press Ctrl+R to restart.", ""},
             sound = "chat_very_bad",
+            color = {255/255, 220/255, 220/255},
 			last = true,
 		},
 	},
@@ -145,7 +147,7 @@ local chatProgression = {
         sound = "chat_good",
         },
     unlock_research2 = {
-        text = {"The scientists have invented the", "sleep-in lab - expected to boost", "reasearch by 150%. Management has",  "mandated their use.", ""},
+        text = {"The scientists have invented the", "sleep-in lab - expected to boost", "reasearch by 150%. Management", "has mandated their use.", ""},
         sound = "chat_good",
     },
     unlock_light = {
